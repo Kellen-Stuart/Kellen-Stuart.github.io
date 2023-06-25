52741ba776c5972d838b2bae7d67e9cbcf5a7d14
Kellen Stuart <kellen.m.stuart@gmail.com>
2023-06-25 15:46:57 -0600

    remove the cookie banner from Calendly

--- calendly.js
+++ calendly.js
@@ -1,7 +1,7 @@
 function initCalendly()
 {
     Calendly.initInlineWidget({
-        url: 'https://calendly.com/kellenstuart',
+        url: 'https://calendly.com/kellenstuart?hide_gdpr_banner=1',
         parentElement: document.getElementById('kellen-calendly'),
         prefill: {},
         utm: {}
