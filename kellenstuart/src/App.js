e3485eb7c4bd3c5943bc0c2bb575a2121c46abef
Kellen Stuart <kellen.m.stuart@gmail.com>
2024-03-30 22:53:47 -0600

    fix routing - use hash router

---
+++ kellenstuart/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Resume from './components/Resume';
+import KSNavbar from './components/KSNavbar';
+import CoverLetter from './components/CoverLetter';
+import Contact from './components/Contact';
+import CharacterSheet from './components/CharacterSheet';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <KSNavbar />
+      <Routes>
+        <Route path="/" element={<CoverLetter />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/character" element={<CharacterSheet />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
