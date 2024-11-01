0898cc8a3ad08ce8949a5d58a11b7d56622412e4
Kellen Stuart <kellen.m.stuart@gmail.com>
2024-11-01 15:17:04 -0600

    Updates

--- precache-manifest.25296bffcf6e4abe89b6e54e1e378b0f.js
+++ precache-manifest.25296bffcf6e4abe89b6e54e1e378b0f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92ea705adda1a4f5f5a0dd00f9668a13",
+    "revision": "6cda8cbe75245e6b68bdc8fc503615e4",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.b71989ea.chunk.css"
   },
   {
-    "revision": "bb8d3b32c58262dd47bb",
+    "revision": "83f31698abb805760276",
     "url": "/static/css/main.2e4be2d8.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.dd3d1c98.chunk.js"
   },
   {
-    "revision": "bb8d3b32c58262dd47bb",
-    "url": "/static/js/main.247a02bb.chunk.js"
+    "revision": "83f31698abb805760276",
+    "url": "/static/js/main.373c7769.chunk.js"
   },
   {
     "revision": "8e7ab2565cd38ae756ad",
